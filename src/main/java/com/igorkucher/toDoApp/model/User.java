7f6bd65096943dce8a92a2fd95344083a261aba3
Ihor Kucher <1672624@gmail.com>
2022-11-13 16:34:22 +0200

    Updated User

--- src/main/java/com/igorkucher/toDoApp/model/User.java
+++ src/main/java/com/igorkucher/toDoApp/model/User.java
@@ -1,4 +1,73 @@
 package com.igorkucher.toDoApp.model;
 
+import javax.persistence.*;
+import javax.validation.constraints.Pattern;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Pattern(regexp = "[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}", message = "Must be a valid e-mail address")
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.REMOVE)
+    private List<ToDo> myTodos;
+
+    @ManyToMany
+    @JoinTable(name = "todo_collaborator",
+            joinColumns = @JoinColumn(name = "collaborator_id"),
+            inverseJoinColumns = @JoinColumn(name = "todo_id"))
+    private List<ToDo> otherTodos;
+
+    public User(){}
+
+    public User(long id, String email) {
+        this.id = id;
+        this.email = email;
+    }
+
+    public List<ToDo> getMyTodos() {
+        return myTodos;
+    }
+
+    public void setMyTodos(List<ToDo> myTodos) {
+        this.myTodos = myTodos;
+    }
+
+    public List<ToDo> getOtherTodos() {
+        return otherTodos;
+    }
+
+    public void setOtherTodos(List<ToDo> todos) {
+        this.otherTodos = todos;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
